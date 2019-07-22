36c7044fb3be3771e64cb93b07f096c3a3a68be7
Antonio Bielma <thetonybl@gmail.com>
2019-07-22 16:55:10 -0500

    All
    All

---
+++ Assets/Scrips/PlayerWalk.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerWalk : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
+    public AudioSource m_audio;
+    private int numItemns = 9;
+    //public AudioCLip m_pickUpSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+          rb = GetComponent<Rigidbody>();
+        count = 0;
+        countText.text = "Count: " + count.ToString();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1")){
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+    }
+      private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("PickUp"));
+        {
+            other.gameObject.SetActive(false);  
+            count = count + 1;
+            countText.text = "Count: " + count.ToString();
+            m_audio.Play();
+            if(count >= numItemns)
+            {
+                winText.text = "Felicidades, has recogido todos!";
+            }
+        }
+    }
+}
